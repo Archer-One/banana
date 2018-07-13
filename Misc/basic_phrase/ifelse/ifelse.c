a87b0d589757e2b78ff728fdb6e0cec6616ef42b
B3t4M3Ee <986256128@qq.com>
2018-07-13 22:49:32 +0800

    basic_phrase

---
+++ Misc/basic_phrase/ifelse/ifelse.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a=10;
+if (a==1)
+{
+	printf("1");
+}
+else if (a==2)
+{
+	printf("2");
+}
+else 
+{
+	printf("3");
+}
+}
---
+++ Misc/basic_phrase/switch/switch.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a=10;
+switch(a){
+	case 0 : puts("1");break;
+	case 1 : puts("2");break;
+	case 3 : puts("3");break;
+	case 4 : puts("4");break;
+	default: puts("5");
+
+	}
+}
